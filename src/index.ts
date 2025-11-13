05829f19b54909cea7a8eaededc6ed267a3c36e7
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-11-13 18:56:34 +0100

    Add Prisma client and test DB route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 //const place
 const PORT = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
+//root
 app.get("/", (req, res) => {
   res.json({ message: "File uploader ready!" });
 });
 
+//test route
+app.get("/test-db", async (req, res) => {
+  const userCount = await prisma.user.count();
+  res.json({ message: "DB connected!", users: userCount });
+});
+
+//check port
 app.listen(PORT, () => {
   console.log("Server running on http://localhost:3000");
 });
